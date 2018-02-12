b593c2aa0ffc80df680db4cb8e91de57249dc54e
indiedriver <amathias63@gmail.com>
2018-02-12 15:11:24 +0100

    chunk initialisation

--- src/chunk.cpp
+++ src/chunk.cpp
@@ -1,5 +1,9 @@
 #include "chunk.hpp"
 
+std::vector<glm::vec3> plane_vertices = {
+    {-1.0f, 1.0f, 0.0f}, {1.0f, 1.0f, 0.0f},   {1.0f, -1.0f, 0.0f},
+    {1.0f, -1.0f, 0.0f}, {-1.0f, -1.0f, 0.0f}, {-1.0f, 1.0f, 0.0f}};
+
 Chunk::Chunk(void) {
   for (int y = 0; y < 256; y++) {
     for (int x = 0; x < 16; x++) {
@@ -21,6 +25,23 @@ Chunk& Chunk::operator=(Chunk const& rhs) {
   return (*this);
 }
 
+void Chunk::mesh() {
+  std::vector<glm::vec3> vertices;
+  for (int y = 0; y < CHUNK_SIZE; y++) {
+    for (int x = 0; x < CHUNK_SIZE; x++) {
+      for (int z = 0; z < CHUNK_SIZE / 2; z++) {
+        Block front_block = get_block({x, y, z});
+        if (front_block.material != Material::Air) {
+          vertices.insert(vertices.end(), plane_vertices.begin(),
+                          plane_vertices.end());
+          break;
+        }
+      }
+    }
+  }
+  std::cout << "Mesher: " << vertices.size() << " vertices" << std::endl;
+};
+
 inline Block Chunk::get_block(glm::ivec3 index) {
   return (this->data[index.y * CHUNK_SIZE * CHUNK_SIZE + index.x * CHUNK_SIZE +
                      index.z]);
@@ -31,16 +52,21 @@ inline void Chunk::set_block(Block block, glm::ivec3 index) {
              index.z] = block;
 }
 
-ChunkManager::ChunkManager(void) : _renderDistance(4) {
+ChunkManager::ChunkManager(void) : _renderDistance(3) {
   glm::ivec2 pos(0);
-  for (int x = -this->_renderDistance; x < this->_renderDistance; x++) {
-    for (int z = -this->_renderDistance; z < this->_renderDistance; z++) {
-      // glm::ivec2 chunk_pos((pos.x + x) % CHUNK_SIZE, (pos.y + z) %
-      // CHUNK_SIZE);  auto chunk = _chunks.find(pos);
-      // if (chunk == _chunks.end()) {
-      //}
+  for (int x = -this->_renderDistance; x <= this->_renderDistance; x++) {
+    for (int z = -this->_renderDistance; z <= this->_renderDistance; z++) {
+      glm::ivec2 chunk_pos(pos.x + x * CHUNK_SIZE, pos.y + z * CHUNK_SIZE);
+      auto chunk = _chunks.find(chunk_pos);
+      if (chunk == _chunks.end()) {
+        Chunk newChunk;
+        newChunk.mesh();
+        _chunks.insert(std::make_pair(chunk_pos, newChunk));
+      }
     }
   }
+  std::cout << "Chunk manager load: " << _chunks.size() << " chunks"
+            << std::endl;
 }
 
 ChunkManager::ChunkManager(ChunkManager const& src) { *this = src; }
--- src/chunk.hpp
+++ src/chunk.hpp
@@ -1,15 +1,31 @@
 #pragma once
 #define CHUNK_SIZE 16
+#define MODEL_HEIGHT 16
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
-#include <glm/gtx/hash.hpp>
+//#include <glm/gtx/hash.hpp>
 #include <iostream>
 #include <map>
+#include <unordered_map>
+#include <vector>
+
+struct ivec2Comparator {
+  size_t operator()(const glm::ivec2& k) const {
+    size_t h = std::hash<int>()(k.x) ^ (std::hash<int>()(k.y) << 1);
+    return (h);
+  }
+
+  bool operator()(const glm::ivec2& a, const glm::ivec2& b) const {
+    return (a.x == b.x && a.y == b.y);
+  }
+};
 
 enum class Material : unsigned char { Air, Dirt, Sand };
 
 struct Block {
   enum Material material;
+  bool operator==(const Block& rhs) const { return material == rhs.material; };
+  bool operator!=(const Block& rhs) const { return material != rhs.material; };
 };
 
 class Chunk {
@@ -21,6 +37,8 @@ class Chunk {
 
   Block data[65536] = {};
   glm::ivec3 pos;
+
+  void mesh();
 
   inline Block get_block(glm::ivec3 index);
   inline void set_block(Block block, glm::ivec3 index);
@@ -38,5 +56,5 @@ class ChunkManager {
 
  private:
   unsigned char _renderDistance;
-  std::map<glm::ivec2, Chunk> _chunks;
+  std::unordered_map<glm::ivec2, Chunk, ivec2Comparator> _chunks;
 };
